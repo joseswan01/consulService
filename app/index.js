3acfbb4b47601ec7f692ec4ead0b61cda751a07e
joseswan01 <142055254+joseswan01@users.noreply.github.com>
2023-08-24 17:03:58 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const SERVICE_NAME='microservicio';
 const SERVICE_ID='m'+process.argv[2];
 const SCHEME='http';
-const HOST='193.168.100.3';
+const HOST='193.168.100.5';
 const PORT=process.argv[2]*1;
 const PID = process.pid;
 
